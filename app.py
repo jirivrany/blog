2437273231ea73c59dd9633d35c75fe50d295fe9
Jiri Vrany <jiri.vrany@tul.cz>
2014-01-23 10:27:58 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,45 @@
+# -*- coding: utf8 -*-    
+'''
+co chci?
+minimalistický systém 
+markdown pro zápis
+syntax highlite pro zdrojáky
+permalinky jeden-clanek-jaro-nedela
+štítky
+vyzkoušet si mongoDB ?
+cache - https://github.com/thadeusb/flask-cache/ ?
+frozen flask ? statická stránka
+
+'''
+
+import flask
+import markdown
+import codecs
+
+app = flask.Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hiorme():
+    input_file = codecs.open("pages/python/pokus.markdown", mode="r", encoding="utf-8")
+    text = input_file.read()
+    html = markdown.markdown(text, extensions=['codehilite'])
+    return flask.render_template('index.html', html=html)
+
+@app.route('/<topic>/<filename>/')
+def test_param(topic, filename):
+    try:
+        input_file = codecs.open("pages/{}/{}.markdown".format(topic, filename), mode="r", encoding="utf-8")
+    except IOError:
+        flask.abort(404)
+    else:        
+        text = input_file.read()
+        html = markdown.markdown(text, extensions=['codehilite'])
+        return flask.render_template('index.html', html=html)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return flask.render_template('error_404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)       
---
+++ pages/python/pokus.markdown
@@ -0,0 +1,28 @@
+An h1 header
+============
+
+Paragraphs are separated by a blank line.
+
+Šíleně žluťoučký kůň, úpěl ďábelské ódy. Jestli zvládne tohle, tak umí česky...
+
+2nd paragraph. *Italic*, **bold**, `monospace`. Itemized lists
+look like:
+
+  * this one
+  * that one
+  * the other one
+
+a neco
+
+    :::python
+    import hashlib
+    from os import urandom
+    from base64 import b64encode, b64decode
+    from itertools import izip
+
+    app.route('/admin/')
+    def about():
+        data = range(1, 10)
+        return render_template('admin.html', data=data)
+
+
